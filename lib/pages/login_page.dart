f2dc5e5e16b664d450571eb3ad068b3e31ea2572
Emanoel <caio_no@hotmail.com>
2022-09-04 22:39:14 -0300

    Criando pagina de Login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 20, top: 80, right: 20, bottom: 40),
+        child: Text('Olá'),
+      ),
+    );
+  }
+}
